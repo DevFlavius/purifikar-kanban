a47166ac1d64097d42794de3ae413bf1d7e5df8a
FlavioPurifikar <flavio.eduardo@purifikar.com.br>
2025-06-04 11:43:37 -0300

    feat: create Header component with logo and title for Kanban board

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../App.css';
+
+const Header = () => {
+  return (
+    <header className="kanban-header">
+        <img src="https://purifikar.com.br/wp-content/uploads/2023/07/logo-pfk-R-hotizontal-branca.png" alt="Purifikar Logo" className="kanban-logo" />
+        <h1 className="kanban-title">Painel de Produção - Purifikar</h1>
+    </header>
+  );
+};
+
+export default Header;
